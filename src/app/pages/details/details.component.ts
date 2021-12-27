5d33541e80bcecd0abecdf484e79bd11b687d181
jvrodrigs <jvrodrigs@gmail.com>
2021-12-27 15:44:54 -0300

    page details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public isError: boolean = false;
+  
+  constructor(
+    private ActivatedRouter: ActivatedRoute,
+    private pokeService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  public getPokemon(){
+    const id = this.ActivatedRouter.snapshot.params['id'];
+    const detail = this.pokeService.getInfosPokemons(`${this.urlPokemon}/${id}`);
+    const species = this.pokeService.getInfosPokemons(`${this.urlName}/${id}`);
+
+    //Faz um array de URL's e fazendo multiplos request juntos. (forkJoin)
+    forkJoin([detail, species]).subscribe(
+      res => {
+        this.pokemon = res;
+        console.log(this.pokemon);
+        
+        this.isLoading = true;
+      },
+      error => {
+        this.isError = true;
+      }
+    );
   }
 
 }
